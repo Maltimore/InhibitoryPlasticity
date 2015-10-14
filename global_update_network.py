0868a2918667cd3e39b182997ce8a2a8d7533e9a
maltimore <malte.esders@gmail.com>
2015-10-14 17:42:29 +0200

    implemented firing rate counting from neuron objects (not synapses)

--- global_update_network.py
+++ global_update_network.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 start_scope()
 
 ### PARAMETERS ################################################################
-NE = 1000               # Number of excitatory cells
+NE = 400               # Number of excitatory cells
 NI = NE/4               # Number of inhibitory cells
 tau_ampa = 5.0*ms       # Glutamatergic synaptic time constant
 tau_gaba = 10.0*ms      # GABAergic synaptic time constant
 epsilon = 0.02          # Sparseness of synaptic connections
 tau_stdp = 20*ms        # STDP time constant
-simtime = 700*ms       # Simulation time
-rate_interval = 200*ms  # bin size to compute firing rate
+simtime = 700*ms        # Simulation time
+rate_interval = 200     # bin size to compute firing rate
 
 gl = 10.0*nS            # Leak conductance
 el = -60*mV             # Resting potential
@@ -25,7 +25,8 @@ firing_rate_list = []
 
 ### FUNCTIONS #################################################################
 def get_n_of_spikes(spike_time_vec, t, intervallength):
-    n_spikes = len(spike_time_vec[t-intervallength < spike_time_vec < t])
+    n_spikes = len(spike_time_vec[(t-intervallength < spike_time_vec) & \
+                                  (spike_time_vec < t)])
     return n_spikes
 ### NEURONS ###################################################################
 print("Creating neurons..")
@@ -52,51 +53,39 @@ con_ii = Synapses(Pi, Pi, pre='g_gaba += 3*nS', connect='rand()<epsilon')
 print("Creating plastic synapses..")
 con_ei = Synapses(Pi, Pe,
                   model='''w : 1
-                           pre_spikes_last_second : 1
-                           pre_spikes_last_interval : 1
                            ''',
-                  pre='''pre_spikes_last_second += 1
-                         pre_spikes_last_interval += 1
-                         g_gaba += w*nS
+                  pre='''g_gaba += w*nS
                          w += 1e-11                      
                          ''',
                   connect='rand()<epsilon')
 con_ei.w = 300
-con_ei.pre_spikes_last_second = 0
-con_ei.pre_spikes_last_interval = 0
-con_ei.run_regularly("""pre_spikes_last_second = 0""", dt=1000*ms)
 
 ### MONITORS ##################################################################
 print("Setting up Monitors..")
-StateMon = StateMonitor(con_ei, ['w', 'pre_spikes_last_second'], record=0)
+#StateMon = StateMonitor(con_ei, ['w'], record=0)
 SpikeMon = SpikeMonitor(neurons)
-inh_SpikeMon = SpikeMonitor(Pi)
+inhSpikeMon = SpikeMonitor(Pi)
 excStateMon = StateMonitor(Pe, "v", record=0)
 inhStateMon = StateMonitor(Pi, "v", record=0)
 
 ### ARBITRARY PYTHON CODE #####################################################
-@network_operation(dt=1000*ms)
-def global_update():
-    cum_sum = np.sum(con_ei.pre_spikes_last_second)
-    print("cumulative sum is: " + str(cum_sum))
-    con_ei.pre_spikes_last_second = 0 # reset counter
-    
-@network_operation(dt=rate_interval)
-def compute_firing_rate(t):
-    t = t/ms
+@network_operation(dt=rate_interval*ms)
+def compute_inh_firing_rate(t):
     print("compute_firing_rate called at t = " + str(t))
+    t = t/ms
+    spike_times = inhSpikeMon.t/ms
     if t == 0:
-        print("skipping first step")
+        # if this is t = 0, skip the computation
         return
-    firing_rate = np.sum(con_ei.pre_spikes_last_interval) \
-    / rate_interval * ms / len(con_ei) * 1000
-    con_ei.pre_spikes_last_interval = 0 # reset counter
+    firing_rate = get_n_of_spikes(spike_times, t, rate_interval)/     \
+                  (rate_interval / 1000 * NI)
     firing_rate_list.append([t, firing_rate])
     
 ### NETWORK ###################################################################
 print("Creating Network..")
-MyNet = Network(neurons, Pe, Pi, con_e, con_ii, con_ei, StateMon, inhStateMon,
-                excStateMon, SpikeMon, global_update, compute_firing_rate)
+MyNet = Network(neurons, Pe, Pi, con_e, con_ii, con_ei, inhStateMon,
+                excStateMon, SpikeMon, compute_inh_firing_rate,
+                inhSpikeMon)
     
 ### SIMULATION ################################################################
 print("Running simulation..")
@@ -112,30 +101,23 @@ plt.plot(SpikeMon.t/ms, SpikeMon.i, '.k')
 plt.xlabel("Time (ms)")
 plt.ylabel("Neuron index")
 
-show_n = 10
+#inhibitory spikes
+show_n = 20
 plt.figure()
-plt.plot(SpikeMon.t/ms, SpikeMon.i, '.k')
-plt.ylim([NE, NE+10])
+plt.plot(inhSpikeMon.t/ms, inhSpikeMon.i, '.k')
+plt.ylim([0, show_n])
 plt.xlabel("Time (ms)")
 plt.ylabel("Neuron index")
 plt.title("Spikes in the first " + str(show_n) + " inhibitory neurons")
 
-
 # synaptic weight
-plt.figure()
-plt.plot(StateMon.t/ms, StateMon.w.T)
-plt.xlabel("time [ms]")
-plt.ylabel("weight")
-plt.title("Synaptic weight of inhibitory synapse #1")
+#plt.figure()
+#plt.plot(StateMon.t/ms, StateMon.w.T)
+#plt.xlabel("time [ms]")
+#plt.ylabel("weight")
+#plt.title("Synaptic weight of inhibitory synapse #1")
 
-plt.figure()
-plt.plot(StateMon.t/ms, StateMon.pre_spikes_last_second.T)
-plt.xlabel("time [ms]")
-plt.ylabel("# Spikes")
-plt.title("Spikes this bin in the presynaptic neuron of inhibitory synapse #1")
-plt.xlim([-10, np.amax(StateMon.t/ms)])
-plt.ylim([0, np.amax(StateMon.pre_spikes_last_second)+1])
-
+# Voltage traces
 plt.figure()
 plt.plot(inhStateMon.t/ms, inhStateMon.v.T/volt, label="inh #1")
 plt.plot(excStateMon.t/ms, excStateMon.v.T/volt, label="exc #1")
@@ -144,9 +126,9 @@ plt.ylabel("Voltage")
 plt.legend()
 plt.title("Voltage traces")
 
+# Firing rate
 plt.figure()
 plt.plot(firing_rate_list[0,:], firing_rate_list[1,:])
-#plt.ylim([0, np.amax(firing_rate_list[1,:])])
 plt.xlim([0, firing_rate_list[0,-1]])
 plt.xlabel("time [ms]")
 plt.ylabel("firing rate [Hz]")
